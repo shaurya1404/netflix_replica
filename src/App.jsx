2db3816a136b29b0f3281bf71a41c05a566013a1
Shaurya <shaurya.sharma1404@gmail.com>
2024-03-09 12:52:40 -0500

    added create user and authentication functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,26 @@ import HomeScreen from './screens/HomeScreen'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
 import LoginScreen from './screens/LoginScreen';
+import { useEffect } from 'react';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/9.0.2/firebase-auth.js';
+const auth = getAuth();
 
 function App() {
 
   const user = null;
+
+  useEffect(() => {
+    const toUnmount = auth.onAuthStateChanged(userAuth => {
+      if(userAuth) {
+        //Logged In
+        console.log(userAuth)
+      }
+      else {
+        //Logged Out
+      }
+    });
+    return toUnmount; // cleaning up by using "return" keyword in use Effect to remove the listener onAuthStateChanged 
+  })
 
   return (
       <div className='app'>
--- src/screens/SignUpScreen.jsx
+++ src/screens/SignUpScreen.jsx
@@ -1,9 +1,60 @@
+import { useRef } from "react";
 import "./SignUpScreen.css"
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.0.2/firebase-auth.js';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAfuqXZG7CbhUvRxknZyFUTYMNbAk6E4g0",
+    authDomain: "netflix-clone-8b56a.firebaseapp.com",
+    projectId: "netflix-clone-8b56a",
+    storageBucket: "netflix-clone-8b56a.appspot.com",
+    messagingSenderId: "634021542859",
+    appId: "1:634021542859:web:61f8b19f96fb44f553544e"
+  };
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
 
 export default function SignUpScreen() {
+
+    const register = (event) => {
+        event.preventDefault(); // to prevent page from refershing when a button is clicked
+        createUserWithEmailAndPassword(
+            auth,
+            emailRef.current.value,
+            passwordRef.current.value
+        )
+        .then((createdUser) => { console.log(createdUser)})
+        .catch(error => alert(error.message))
+    };
+
+    const signIn = (event) => {
+        event.preventDefault();
+        signInWithEmailAndPassword(
+            auth,
+            emailRef.current.value,
+            passwordRef.current.value
+        )
+        .then(signedInUser => console.log(signedInUser))
+        .catch(error => alert(error.message))
+    };
+
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null)
+
     return (
         <div className="signupScreen">
-
+            <form>
+                <h1>Sign In</h1>
+                <input ref={emailRef} placeholder="Email" type="email"></input>
+                <input ref={passwordRef} placeholder="Password" type="password"></input>
+                <button type="submit" onClick={signIn}>Sign In</button>
+                <h4>
+                    <span className="signupScreen_gray">New to Netflix? </span> 
+                    <span className="signupScreen_link" onClick={register}>Sign up now.</span>
+                </h4>
+            </form>
         </div>
     )
 }
