e635b23c10211e479acc631e02e273017d16a587
Shaurya <shaurya.sharma1404@gmail.com>
2024-03-11 03:40:44 -0400

    final changes and ready to deploy

--- src/screens/ProfileScreen.jsx
+++ src/screens/ProfileScreen.jsx
@@ -18,13 +18,28 @@ export default function ProfileScreen() {
                 <div className="profileScreen_info">
                     <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"></img>
                     <div className="profileScreen_details">
-                        <div className="profileScreen_plans">
-                            <h3>Plans</h3>
-                        </div>
                         <h2>{user.email}</h2>
-                        <div className="profileScreen_plans">
-                            <button onClick={() => auth.signOut()} className="profileScreen_signOut">Sign Out</button>
+                        <h3>Plans</h3>
+                    </div>
+                </div>
+                <div className="profileScreen_subs">
+                    <h4>Renewal Date: 09/05/2024</h4><br></br>
+                    <div className="profileScreen_plansBox">
+                        <div className="box1">
+                            <h4>Netflix Basic</h4>
+                            <button className="box_btn">Change Plan</button>
                         </div>
+                        <div className="box2">
+                            <h4>Netflix Premium</h4>
+                            <button className="box_btn">Change Plan</button>
+                        </div>
+                        <div className="box3">
+                            <h4>Netflix Family</h4>
+                            <button className="box_btnSubs">Current Plan</button>
+                        </div>
+                    </div>
+                    <div className="profileScreen_plans">
+                            <button onClick={() => auth.signOut()} className="profileScreen_signOut">Sign Out</button>
                     </div>
                 </div>
             </div>
